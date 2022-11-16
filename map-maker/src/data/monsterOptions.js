79d51ea154d5828f8d68e4ca4a13af9d35cb0b55
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-11-16 10:37:21 +0100

    A shamefully big commit.

---
+++ map-maker/src/data/monsterOptions.js
@@ -0,0 +1,69 @@
+export const MONSTER_OPTIONS = [{
+    cellName: "Bandit Chief",
+    cellSize: "medium",
+    cellUrl: "https://www.kryxrpg.com/static/a5ef4a2187298abc4de31ba3f0fca6c7/5aa84/bandit.avif"
+}, {
+    cellName: "Bandit",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1039964412703952976/My_project_2.png"
+}, {
+    cellName: "Berserker",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1039961944397975652/berserker_main.png"
+}, {
+    cellName: "Beholder",
+    cellSize: "large",
+    cellUrl: "https://www.kryxrpg.com/static/b5bfe3f8f12161cc1a1d23eb5341538b/49e7e/beholder.avif"
+}, {
+    cellName: "Vine Blight",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1039963498828013669/My_project_1.png"
+},{
+    cellName: "Needle Blight",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1039972197688684574/LCo1m3W.png"
+}, {
+    cellName: "Twig Blight",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1039973874755965039/b55ade582bfae8db0709300edcc6e265.png"
+},{
+    cellName: "Ankeg",
+    cellSize: "large",
+    cellUrl: "https://www.kryxrpg.com/static/70bbf720a9efc41e64d4c7c2180ece4b/d0137/ankheg.avif"
+}, {
+    cellName: "Spider",
+    cellSize: "medium",
+    cellUrl: "https://www.kryxrpg.com/static/289ba6b41d2e99405cfe75237a5c8080/7bae7/giant-spider.avif"
+}, {
+    cellName: "Displacer",
+    cellSize: "large",
+    cellUrl: "https://www.kryxrpg.com/static/8f7e9536cf444507809fe635485d56e3/fb1ff/displacer-beast.avif"
+},{
+    cellName: "Abominable Yeti",
+    cellSize: "huge",
+    cellUrl: "https://www.kryxrpg.com/static/3110afcfe7d9cd419f79494cd4858067/997b7/abominable-yeti.avif"
+},{
+    cellName: "Adult Green Dragon",
+    cellSize: "huge",
+    cellUrl: "https://www.kryxrpg.com/static/902c85fccb25ff159d3bf23749bae1ed/f321e/adult-green-dragon.avif"
+}, {
+    cellName: "Gargoyle",
+    cellSize: "medium",
+    cellUrl: "https://www.kryxrpg.com/static/0f884530fd915008064e5659bba9a04a/ce86c/gargoyle.avif"
+},{
+    cellName: "Animated Statue",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1039971559336595466/My_project_3.png"
+},{
+    cellName: "Basilisk",
+    cellSize: "medium",
+    cellUrl: "https://www.kryxrpg.com/static/1273915eafe3a93a85101a961d85b01f/4ae3d/basilisk.avif"
+},{
+    cellName: "Helmed Horror",
+    cellSize: "medium",
+    cellUrl: "https://www.kryxrpg.com/static/560115bd161911a239d931d693cbd288/e47d3/helmed-horror.avif",
+},{
+    cellName: "Hell Hound",
+    cellSize: "medium",
+    cellUrl: "https://www.kryxrpg.com/static/295c8956c39f286151c102a2e8b3e7cc/02303/hell-hound.avif",
+}];
---
+++ map-maker/src/data/playerOptions.js
@@ -0,0 +1,51 @@
+export const PLAYER_OPTIONS = [{
+    cellName: "Zah'ir",
+    cellSize: "medium",
+    cellUrl: "https://media.discordapp.net/attachments/1025408325321498625/1038137190816940112/16675815587338544119634098879295.jpg",
+    speed: 6
+}, {
+    cellName: "Howl",
+    cellSize: "medium",
+    cellUrl: "https://i.etsystatic.com/35550803/r/il/03d72d/3846156466/il_794xN.3846156466_qdfe.jpg",
+    speed: 6
+}, {
+    cellName: "Béla",
+    cellSize: "medium",
+    cellUrl: "https://vignette.wikia.nocookie.net/blackbandos-homebrew/images/b/bf/Burning_knight.jpg/revision/latest?cb=20171024132523",
+    speed: 5
+}, {
+    cellName: "Theo",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/943774890509623346/1038144811527585792/Theo_lvl2_screenshot.png",
+    speed: 5
+}, {
+    cellName: "Beni",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/894595052427431936/1039484264330313818/My_project.png",
+    speed: 6
+},{
+    cellName: "Herman",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/894595052427431936/1040272425285341266/My_project_5.png",
+    speed: 5,
+},{
+    cellName: "Aizen",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/1022511328771985469/1040294520350322808/My_project_9.png",
+    speed: 8,
+},{
+    cellName: "Alfina",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1040357402375094353/My_project_11.png",
+    speed: 8,
+},{
+    cellName: "Grug",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/910567793290805289/1040302362289524846/My_project_10.png",
+    speed: 6,
+},{
+    cellName: "Spiritual Weapon",
+    cellSize: "medium",
+    cellUrl: "https://cdn.discordapp.com/attachments/1022511328771985469/1040353290736640070/fire-hammer.png",
+    speed: 4,
+}];
