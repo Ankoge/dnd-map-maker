5ca771ac088d60737cc5c465e87b9e49543a6468
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-12-04 17:06:55 +0100

    Add the global events only at the first load on.

--- map-maker/src/App.js
+++ map-maker/src/App.js
@@ -1,44 +1,50 @@
 import './App.css';
 import Map from "./components/Map";
-import EditorSidebar from "./components/EditorSidebar";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [isMouseDown, setIsMouseDown] = useState(false)
+    useEffect(() => {
+        document.body.addEventListener('keydown', (event) => {
+            const modifyCellSize = (modifier) => {
+                let size = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--cell-size"), 10);
+                if (size > 200) {
+                    size = 200;
+                } else if (size < 0) {
+                    size = 0;
+                }
+                document.documentElement.style.setProperty('--cell-size', `${size + modifier}px`)
+            }
+            switch (event.key) {
+                case "-":
+                    modifyCellSize(-1);
+                    break;
+                case "+":
+                    modifyCellSize(1);
+                    break;
+                default:
+                    break;
+            }
+        })
 
-    document.body.addEventListener('keydown', (event) => {
-        const modifyCellSize = (modifier) => {
-            let size = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--cell-size"), 10)
-            document.documentElement.style.setProperty('--cell-size', `${size + modifier}px`)
-        }
-        switch (event.key) {
-            case "-":
-                modifyCellSize(-1);
-                break;
-            case "+":
-                modifyCellSize(1);
-                break;
-            default:
-                break;
-        }
-    })
-
-    document.body.addEventListener('mouseup', (event) => {
+        document.body.addEventListener('mouseup', (event) => {
 
             setIsMouseDown(false)
 
-    })
+        })
 
-    document.body.addEventListener('mousedown', event => {
-        setIsMouseDown(true)
-    })
+        document.body.addEventListener('mousedown', event => {
+            setIsMouseDown(true)
+        })
+    }, [])
+
 
     return (
         <div className="App">
             <header className="App-header">
             </header>
             <Map mapSize={40}
-            isMouseDown={ isMouseDown}/>
+                 isMouseDown={isMouseDown}/>
         </div>
     );
 }
