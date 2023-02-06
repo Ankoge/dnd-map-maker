e8627132d84681e5d7316643cc4a8b585bb05a83
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2023-02-06 22:54:14 +0100

    Give the possibility to chose between dark and light style.

---
+++ map-maker/src/data/colorOptions.js
@@ -0,0 +1,39 @@
+export const COLOR_OPTIONS = [
+    {
+        name: "Dark",
+        representation: "black",
+        colorTable: {
+            "--font": "rgb(255,255,255)",
+            "--background": "rgb(80, 80, 80)",
+            "--border": "rgb(0, 0, 0)",
+            "--hex-background": "rgba(255, 255, 255, 0.3)",
+            "--hover": "rgb(150, 150, 150)",
+            "--hover-transparent": "rgba(133, 133, 133, 0.5)",
+            "--hex-background-movable": "rgba(154, 225, 157, 0.6)",
+            "--menu-background": "rgb(40, 40, 40)",
+            "--shadow": "rgb(128, 128, 128)",
+            "--icon-background": "rgb(100, 100, 100)",
+            "--chosen": "rgb(0, 0, 0)",
+            "--scrollbar-track": "rgb(110, 110, 110)",
+            "--scrollbar-thumb": "rgb(30, 30, 30)",
+        }
+    }, {
+        name: "Light",
+        representation: "lightgray",
+        colorTable: {
+            "--font": "rgb(0,0,0)",
+            "--background": "rgb(211, 211, 211)",
+            "--border": "rgb(0, 0, 0)",
+            "--hex-background": "rgba(255, 255, 255, 0.3)",
+            "--hover": "rgb(150, 150, 150)",
+            "--hover-transparent": "rgba(133, 133, 133, 0.5)",
+            "--hex-background-movable": "rgba(154, 225, 157, 0.6)",
+            "--menu-background": "rgb(217, 217, 217)",
+            "--shadow": "rgb(128, 128, 128)",
+            "--icon-background": "rgb(255, 255, 255)",
+            "--chosen": "rgb(190, 190, 190)",
+            "--scrollbar-track": "rgb(241, 241, 241)",
+            "--scrollbar-thumb": "rgb(136, 136, 136)",
+        }
+    }
+]
