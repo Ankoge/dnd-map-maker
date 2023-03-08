bfdbe49f5d7b2f4222efda0858ad0fa08b3176d4
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2023-03-08 10:44:53 +0100

    Data simplify.

--- map-maker/src/data/environmentOptions.js
+++ map-maker/src/data/environmentOptions.js
@@ -3,13 +3,13 @@ import {SHAPE_OPTION, SIZE_OPTION} from "./options";
 export const ENVIRONMENT_OPTIONS = [{
     cellName: "Gate",
     cellSize: SIZE_OPTION.MEDIUM,
-    cellShape: "tall-over-sized",
+    cellShape: SHAPE_OPTION.TALL_OVER_SIZED,
     cellTerrain: "unmovable",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1067444427829805177/pngegg_42.png"
 }, {
     cellName: "Dead Cross",
     cellSize: SIZE_OPTION.MEDIUM,
-    cellShape: SHAPE_OPTION.ROUND.name,
+    cellShape: SHAPE_OPTION.ROUND,
     cellTerrain: "hard",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1079434143122870362/dead-cross-resample.png"
 }, {
@@ -27,19 +27,19 @@ export const ENVIRONMENT_OPTIONS = [{
 }, {
     cellName: "Swamp",
     cellSize: SIZE_OPTION.SMALL,
-    cellShape: SHAPE_OPTION.ROUND.name,
+    cellShape: SHAPE_OPTION.ROUND,
     cellTerrain: "hard",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1062739161595002920/pngegg_36.png"
 }, {
     cellName: "Barrel",
     cellSize: SIZE_OPTION.SMALL,
-    cellShape: SHAPE_OPTION.FLAT.name,
+    cellShape: SHAPE_OPTION.FLAT,
     cellTerrain: "unmovable",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1060737062954872832/pngegg_2.png"
 }, {
     cellName: "Pile of Rock",
     cellSize: SIZE_OPTION.MEDIUM,
-    cellShape: SHAPE_OPTION.FLAT.name,
+    cellShape: SHAPE_OPTION.FLAT,
     cellTerrain: "hard",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1060740675517296681/pngegg_4.png"
 }, {
@@ -57,13 +57,13 @@ export const ENVIRONMENT_OPTIONS = [{
 }, {
     cellName: "Campfire",
     cellSize: SIZE_OPTION.SMALL,
-    cellShape: SHAPE_OPTION.ROUND.name,
+    cellShape: SHAPE_OPTION.ROUND,
     cellTerrain: "hard",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1060856581480255579/pngegg_10.png"
 }, {
     cellName: "Weapon Rack",
     cellSize: SIZE_OPTION.SMALL,
-    cellShape: SHAPE_OPTION.TALL.name,
+    cellShape: SHAPE_OPTION.TALL,
     cellTerrain: "unmovable",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1060963651508510841/My_project_21.png"
 }, {
@@ -81,25 +81,25 @@ export const ENVIRONMENT_OPTIONS = [{
 }, {
     cellName: "Trunk",
     cellSize: SIZE_OPTION.TINY,
-    cellShape: SHAPE_OPTION.FLAT.name,
+    cellShape: SHAPE_OPTION.FLAT,
     cellTerrain: "hard",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1060967704825180190/pngegg_27.png"
 }, {
     cellName: "Chair",
     cellSize: SIZE_OPTION.SMALL,
-    cellShape: SHAPE_OPTION.TALL.name,
+    cellShape: SHAPE_OPTION.TALL,
     cellTerrain: "hard",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1060967594376581220/pngegg_26.png"
 }, {
     cellName: "Statue Man",
     cellSize: SIZE_OPTION.SMALL,
-    cellShape: SHAPE_OPTION.TALL.name,
+    cellShape: SHAPE_OPTION.TALL,
     cellTerrain: "unmovable",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1062733941158711296/pngegg_32.png"
 }, {
     cellName: "Statue Woman",
     cellSize: SIZE_OPTION.MEDIUM,
-    cellShape: SHAPE_OPTION.ROUND.name,
+    cellShape: SHAPE_OPTION.ROUND,
     cellTerrain: "unmovable",
     cellUrl: "https://media.discordapp.net/attachments/1039961105046437989/1062734149061984316/pngegg_33.png?width=234&height=644"
 }, {
@@ -117,19 +117,19 @@ export const ENVIRONMENT_OPTIONS = [{
 }, {
     cellName: "Trap",
     cellSize: SIZE_OPTION.SMALL,
-    cellShape: SHAPE_OPTION.ROUND.name,
+    cellShape: SHAPE_OPTION.ROUND,
     cellTerrain: "catch",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1060868108081975326/pngegg_15.png"
 }, {
     cellName: "Rope",
     cellSize: SIZE_OPTION.TINY,
-    cellShape: SHAPE_OPTION.ROUND.name,
+    cellShape: SHAPE_OPTION.ROUND,
     cellTerrain: "movable",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1060969983682826240/pngegg_28.png"
 }, {
     cellName: "Chest",
     cellSize: SIZE_OPTION.SMALL,
-    cellShape: SHAPE_OPTION.ROUND.name,
+    cellShape: SHAPE_OPTION.ROUND,
     cellTerrain: "hard",
     cellUrl: "https://cdn.discordapp.com/attachments/1039961105046437989/1060976549945426102/pngegg_30.png"
 }];
